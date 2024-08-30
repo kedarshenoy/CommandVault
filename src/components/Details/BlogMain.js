0411ac44eabdadd942ca16d7d0dfea0c9426ecf1
kedarshenoy <akedarshenoy@gmail.com>
2024-08-30 23:26:13 +0530

    plan

--- src/components/Details/BlogMain.js
+++ src/components/Details/BlogMain.js
@@ -8,6 +8,9 @@ export default function BlogMain() {
        <h2 className='mainPostHeading'>Name of the Post Goes here</h2>
         <img className='blogmainimg' src={require('../../Assets/content.jpeg')} alt=''/>
         <div className='content'></div>
+        {/* Need to add profile name and photo  */}
+        {/* scroll animation  */}
+      {/* Download option  */}
        </div>
     </div>
   )
